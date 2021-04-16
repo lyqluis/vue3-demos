deda1f26fd5ffb04d004ac2d36bc5ceb6800e09b
xiannvjiadexiaogouzi <GULA@L-deMacBook-Air.local>
2021-04-16 18:02:35 +0800

    add db.json

--- vue3-vite-todo-app/src/components/HelloWorld.vue
+++ vue3-vite-todo-app/src/components/HelloWorld.vue
@@ -1,30 +1,48 @@
 <template>
-  <h1>{{ msg }}</h1>
+	<h1>{{ msg }}</h1>
+	<h2>{{ obj }}</h2>
+	<p>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank"
+			>Vite Documentation</a
+		>
+		|
+		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+	</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+	<button @click="state.count++">count is: {{ state.count }}</button>
+	<p>
+		Edit
+		<code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
 </template>
 
+<script>
+	export default {
+		// data() {
+		// 	return {
+		// 		obj: {
+    //       'a':1
+    //     },
+		// 	}
+		// },
+	}
+</script>
+
 <script setup>
-import { defineProps, reactive } from 'vue'
+	import { defineProps, reactive } from "vue"
+  import axios from 'axios'
 
-defineProps({
-  msg: String
-})
+	defineProps({
+		msg: String
+	})
 
-const state = reactive({ count: 0 })
+	const state = reactive({ count: 0 })
+  const obj = reactive({})
+  const fetchData = axios.get('https://my-json-server.typicode.com/<your-username>/<your-repo>')
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
-}
+	a {
+		color: #42b983;
+	}
 </style>
--- vue3-vite-todo-app/vite.config.js
+++ vue3-vite-todo-app/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+// export default defineConfig({
+export default ({ command, mode }) => {
+  return {
+    css: {},
+    esbuild: {},
+    resolve: {
+      // 别名
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        'components': path.resolve(__dirname, 'src/components'),
+        'views': path.resolve(__dirname, 'src/views'),
+        'routes': path.resolve(__dirname, 'src/routes'),
+        'utils': path.resolve(__dirname, 'src/utils'),
+        'styles': path.resolve(__dirname, 'src/styles'),
+      }
+    },
+    plugins: [vue()]
+  }
+}
